c661b76bf89269c860917cda543e7564e811355e
WoodChipper <dgordon@Dons-MacBook.local>
2018-08-25 20:50:56 -0400

    first commit

---
+++ Window-Shopping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Window-Shopping
+//
+//  Created by Don Gordon on 8/25/18.
+//  Copyright © 2018 DGsolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
